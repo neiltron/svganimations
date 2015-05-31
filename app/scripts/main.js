fa706919c0174ee2d996cc836cb8b348d0943a32
Neil Pullman <neil@descend.org>
2015-05-31 18:21:37 -0400

    many updates. bad commits.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,8 +5,63 @@
 
 (function () {
 
-  var timeline = new TimelineMax();
+  var timeline = new TimelineMax({ ease: Power2.easeIn });
 
-  timeline.staggerFromTo('#Layer_2 path', 5, { attr: { 'stroke-dashoffset': 1000 } }, { attr: { 'stroke-dashoffset': 0 }, repeat: -1, yoyo: true }, 1);
+  TweenMax.set($('#lipsums path'), { attr: { 'stroke-dashoffset': 0 }});
+
+  var getString = function () {
+    return strings[Math.floor(Math.random() * strings.length)];
+  };
+
+  var displayWord = function () {
+    var string = getString(),
+        strLength = string.length;
+
+    for (var i = 0; i < strLength; i++) {
+      var letters = $('svg .' + string[i]),
+          el = letters[Math.floor(Math.random() * letters.length)],
+          path = $(el).find('path').first();
+
+      if (typeof path !== 'undefined') {
+        timeline.fromTo(path, 1, {
+          attr: { 'stroke-dashoffset': -pathLengthOf(path) }
+        }, {
+          attr: { 'stroke-dashoffset': 0 },
+          ease: timeline.vars.ease
+        }, '-= .1');
+      }
+    }
+
+    timeline.to('div.blank', 2, { autoAlpha:0 });
+    clearLetters(0);
+  };
+
+  var clearLetters = function (stagger) {
+    var timing = 1,
+        stagger = (stagger - timing) * -1;
+
+    $('#lipsums path').each(function (i, path) {
+      timeline.to(
+        $(path), timing,
+        {
+          attr: { 'stroke-dashoffset': pathLengthOf($(path)) * -1 },
+          ease: timeline.vars.ease
+        },
+        '-=' + stagger
+      );
+    });
+
+    timeline.to('div.blank', 2, { autoAlpha:0, onComplete: function () {
+      displayWord();
+    } });
+  };
+
+  var pathLengthOf = function (el) {
+    if (typeof el[0] !== 'undefined' && typeof el[0].getTotalLength === 'function') {
+      return el[0].getTotalLength();
+    }
+  }
+
+  clearLetters(.025);
 
 })(jQuery);
---
+++ app/scripts/strings.js
@@ -0,0 +1,12 @@
+var strings = [
+  'alpacas',
+  'droopiestnips',
+  'soiledpants',
+  'imitationmeats',
+  'saturated',
+  'adulteraisin',
+  'saraconnor',
+  'crapdonut',
+  'aaasitonit',
+  'ameliorate'
+];
